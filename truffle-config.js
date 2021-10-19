08f256102219dfe082d9540611c12e448c3b7fcf
Marcelo Solovey <msolovey@gmail.com>
2021-10-19 03:09:48 -0300

    Carnisman´s supply chain exercise

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,14 @@ module.exports = {
   networks: {
     local: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     }
   }
   // // To run contract with the latest compiler, uncomment lines 10-14 below:
-  // compilers: { 
-  //   solc: {
-  //     version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
-  //   }
-  // }
+//   compilers: { 
+//     solc: {
+//       version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
+//     }
+//   }
 };
